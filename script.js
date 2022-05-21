63ab5221be81dcaae213b24d6f509028136eedfd
Ladi Grillo <Ladigrillo48@gmail.com>
2022-05-21 17:25:35 +0100

    I updated the submit to only after a rating has been given and i modified the rate component so only the selected component will have a orange color

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ const submit = document.getElementsByClassName('submission');
 const thanks = document.getElementsByClassName('container2');
 const main = document.getElementsByClassName('container')
 const chosen = document.getElementsByClassName('chose')
-
+let selected = false;
 
 
 
  for (let i = 0; i < btns.length; i++){
     btns[i].addEventListener('click', () =>{
-        btns[i].classList.toggle('click')
-
-          chosen[0].innerText = `You selected ${btns[i].id} out of 5`
+        selected = true;
+        [...btns].forEach(btn => btn.classList.remove('click'));
+            btns[i].classList.add('click');
+               chosen[0].innerText = `You selected ${btns[i].id} out of 5`
    
 })    
 };
 
 
 submit[0].addEventListener('click', () =>{
+    if (selected){
     main[0].style.display ='none'
-    thanks[0].style.display = 'grid'
-    
+    thanks[0].style.display = 'grid'  
+    } else{
+        alert("please give us a rating")
+    }
 });
